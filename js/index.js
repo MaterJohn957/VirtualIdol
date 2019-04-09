a9c105b39e2e265356c02754c4df9f224911cdff
MaterJohn957 <1229705525@qq.com>
2019-04-09 16:07:15 +0800

    4.9临时

--- js/index.js
+++ js/index.js
@@ -46,3 +46,104 @@ $('.carousel').carousel({
 //     showDiv.style.display = 'block';
 //     showDiv.innerHTML = '';
 // }
+
+
+/**
+ * 音乐专区和小说专区的控制js代码
+ */
+$(function(){
+
+    // 鼠标进入音乐榜区
+    $("#tf-music").hover(function(){
+        $(this).find(".left-btn").animate({left:"0"},500);
+        $(this).find(".right-btn").animate({right:"0"},500);
+    },
+    function(){
+        $(this).find(".left-btn").animate({left:"-100px"},500);
+        $(this).find(".right-btn").animate({right:"-100px"},500);
+    })
+
+    // 音乐部分导航变换
+    $("#music-tab-hot").click(function(){
+        $("#music-tab-new").removeClass("tab-on");
+        $(this).addClass("tab-on");
+    })
+
+    $("#music-tab-new").click(function(){
+        $("#music-tab-hot").removeClass("tab-on");
+        $(this).addClass("tab-on");
+    })
+
+    // 音乐封边的变化
+    $(".music-cover").hover(function(){
+        $(this).find(".cover-overlay-black").css("display","inline");
+        $(this).find(".cover-play-icon").animate({opacity:"0.9", padding:"25px"},250);
+        $(this).find(".music-cover-img").animate({width:"90px",height:"90px",marginTop:"-5px",marginLeft:"-5px"},250);
+    },
+    function(){
+        $(this).find(".cover-overlay-black").css("display","none");
+        $(this).find(".cover-play-icon").animate({opacity:"0", padding:"30px"},250);
+        $(this).find(".music-cover-img").animate({width:"80px",height:"80px",marginTop:"0px",marginLeft:"0px"},250);
+    })
+
+
+
+    // 初始化小说列表
+    
+   
+
+    for(var i=1; i<=10 ;i++){
+        var novelItem = '.novel-item[data-target="' + i + '"]';
+        $(novelItem).find(".novel-item-num").text(i);
+        if(i === 1){
+            $('.novel-item').find(".novel-cover").css("display","none");
+            $('.novel-item').find(".author").css("display","block");
+        
+            $(novelItem).find(".novel-cover").css("display","block");
+            $(novelItem).find(".author").css("display","none");
+            $(novelItem).find(".novel-item-num").css({"backgroundColor":"#e91b1b","color":"#fff"});
+            continue;
+        }
+        if(i=== 2){
+            $(novelItem).find(".novel-item-num").css({"backgroundColor":"#fc770a","color":"#fff"});
+            continue;
+        }
+        if(i===3){
+            $(novelItem).find(".novel-item-num").css({"backgroundColor":"#fffb00","color":"#fff"});
+            continue;
+        }
+    }
+
+    // 鼠标移动到小说列表中的一个元素上时
+    $("#column-1 .novel-item").mouseenter(function(){
+        $("#column-1 .novel-item").removeClass("novel-on");
+        $(this).addClass("novel-on");
+
+        $('#column-1 .novel-item').find(".novel-cover").css("display","none");
+        $('#column-1 .novel-item').find(".author").css("display","block");
+
+        $(this).find(".novel-cover").css("display","block");
+        $(this).find(".author").css("display","none");
+    })
+    $("#column-2 .novel-item").mouseenter(function(){
+        $("#column-2 .novel-item").removeClass("novel-on");
+        $(this).addClass("novel-on");
+
+        $('#column-2 .novel-item').find(".novel-cover").css("display","none");
+        $('#column-2 .novel-item').find(".author").css("display","block");
+
+        $(this).find(".novel-cover").css("display","block");
+        $(this).find(".author").css("display","none");
+    })
+    $("#column-3 .novel-item").mouseenter(function(){
+        $("#column-3 .novel-item").removeClass("novel-on");
+        $(this).addClass("novel-on");
+
+        $('#column-3 .novel-item').find(".novel-cover").css("display","none");
+        $('#column-3 .novel-item').find(".author").css("display","block");
+
+        $(this).find(".novel-cover").css("display","block");
+        $(this).find(".author").css("display","none");
+    })
+})
+
