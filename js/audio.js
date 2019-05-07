bcc9a00845d036bd1b009060e63999e32455e445
MaterJohn957 <1229705525@qq.com>
2019-05-07 17:40:49 +0800

    5.7 17:00

--- js/audio.js
+++ js/audio.js
@@ -2,3 +2,13 @@
  * Vue 代码
  */
 new Vue({ el: '#navbar' })
+
+
+/**
+ * 点击播放按钮后小球的移动控制代码
+ */
+function playMusic() {
+    document.getElementById('current-time').value=c
+    c=c+1
+    t=setTimeout("timedCount()",1000)
+}
